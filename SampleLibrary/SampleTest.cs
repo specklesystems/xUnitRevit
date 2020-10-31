e0903c7869f1dc9ca907fc7b16ed55ed5bf156ec
Matteo Cominetti <matteo@cominetti.org>
2020-10-31 18:10:58 +0000

    feat: adds method to run action wihtout transaction

--- SampleLibrary/SampleTest.cs
+++ SampleLibrary/SampleTest.cs
@@ -1,7 +1,10 @@
 ﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.IFC;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Xunit;
 using xUnitRevitUtils;
 
@@ -20,7 +23,7 @@ namespace SampleLibrary
 
       var walls = new FilteredElementCollector(doc).WhereElementIsNotElementType().OfCategory(BuiltInCategory.OST_Walls).ToElements();
 
-      foreach(var wall in walls)
+      foreach (var wall in walls)
       {
         var volumeParam = wall.get_Parameter(BuiltInParameter.HOST_VOLUME_COMPUTED);
         Assert.NotNull(volumeParam);
@@ -33,6 +36,37 @@ namespace SampleLibrary
     {
       var feet = UnitUtils.ConvertToInternalUnits(3000, DisplayUnitType.DUT_MILLIMETERS);
       Assert.Equal(5, feet);
+    }
+
+    [Fact]
+    public void GetWallGrossAreaAndRollBack()
+    {
+      var testModel = Utils.GetTestModel("walls.rvt");
+      var doc = xru.OpenDoc(testModel);
+      var walls = new FilteredElementCollector(doc).WhereElementIsNotElementType().OfCategory(BuiltInCategory.OST_Walls).ToElements();
+      var wall = walls[0] as Wall;
+      double grossArea = 0;
+
+      var inserts = wall.FindInserts(true, true, true, true);
+      xru.Run(() =>
+      {
+        using (Transaction transaction = new Transaction(doc, "Temporary - only to get gross area"))
+        {
+          transaction.Start();
+          foreach (ElementId insertId in inserts) { doc.Delete(insertId); }
+          doc.Regenerate();
+          var wallFaceReference = HostObjectUtils.GetSideFaces(wall, ShellLayerType.Exterior);
+          var face = doc.GetElement(wallFaceReference.First()).GetGeometryObjectFromReference(wallFaceReference.First()) as PlanarFace;
+          var wallFaceEdges = face.GetEdgesAsCurveLoops();
+          grossArea = ExporterIFCUtils.ComputeAreaOfCurveLoops(wallFaceEdges);
+          transaction.RollBack();
+
+        }
+      }, doc).Wait();
+
+
+
+      Assert.True(grossArea > 0);
     }
 
 
